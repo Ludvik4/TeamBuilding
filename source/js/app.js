2df0b47a7ed5050535256646832f7b50f77aaa9c
Ekaterina Pustovaya <krobox@gmail.com>
2017-03-02 09:55:52 +0300

    verstka 1.0.0 - done

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,12 @@
+//маска на input для  ввда телефона
+$(function() {
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+});
+
+// ==============  Форма Получить СПЕЦТАРИФ
 $(function(){
     $(".form-spectarif__btn").click(function(e) { //устанавливаем событие отправки для формы
-
         e.preventDefault();
-
 
         var form = $(this).parent('form');
         var ph = $('.form-spectarif__inp-phone');
@@ -25,7 +29,9 @@ $(function(){
                     $(".section-form-title").html("Спасибо за обращение, менеджер свяжется  с вами в ближайшее время");
                     $(form).hide();
                     //код в этом блоке выполняется при успешной отправке сообщения
-
+                },
+                error : function () {
+                    $(".section-form-title").html("Ошибка отправки");
                 }
             });
         }
@@ -36,26 +42,22 @@ $(function(){
 });
 
 
-//Форма - обратный звонок по клику на номер телефона
+//==========================Форма - обратный звонок по клику на номер телефона
+
 $(function() {
-
-    $('input[name=phone]').mask("+7 (999) 999-99-99");
-
-    if($(window).width() >= 800) {
-        $('#phone-form_link').click( function(event){  //открытие модального окна
-             var where = $(this).attr('data-where');
-            event.preventDefault();
-            $('#overlay').fadeIn(400,
-                function(){
-                    $('.phone-form_wrap')
-                        .css('display', 'block')
-                        .animate({opacity: 1, top: '50%'}, 200);
-                    $('#phone-form').find('input[name=where]').val(where);
-                });
-        });
-    }
-
-
+        if($(window).width() >= 800) { //срабатывает при ширине экрана больше 800px
+            $('#phone-form_link').click( function(event){  //открытие модального окна
+                 var where = $(this).attr('data-where');
+                event.preventDefault();
+                $('#overlay').fadeIn(400,
+                    function(){
+                        $('.phone-form_wrap')
+                            .css('display', 'block')
+                            .animate({opacity: 1, top: '50%'}, 200);
+                        $('#phone-form').find('input[name=where]').val(where);
+                    });
+            });
+        }
     /* Зaкрытие мoдaльнoгo oкнa */
     $('#phone-forml_close, #overlay').click( function(){
         $('.phone-form_wrap')
@@ -66,57 +68,48 @@ $(function() {
                 }
             );
     });
-
     //отправка формы
-
-    $('.phone-form_btn').on('click', function(e){
+    $(".phone-form_btn").click(function(e) { //устанавливаем событие отправки для формы
         e.preventDefault();
-
-        var ph = $('.phone-form_inp[name=phone]');
+        var form = $(this).parent('form');
+        var ph = $('.phone-form_inp');
         var ph_ = ph.val();
 
         if (ph_!='') {
             $('.phone-form_inp').css({'border': '0 none'});
+
+            var form_data = form.serialize(); //собираем все данные из формы
             $.ajax({
-                url  : '/feedback/phone_click_form',
-                data : $('#phone-form').serialize(),
-                type : 'POST',
-                beforeSend : function()
-                {
+                type: "POST", //Метод отправки
+                url: "http://maot.ru/podmsk/tilda/les_landing.php", //путь до php фаила отправителя
+                data: form_data,
+                beforeSend: function () {
                     $(".phone-form_btn").attr('disabled', true);
                     $(":submit", this).attr("disabled", "disabled");
                     $(".phone-form_title").html('Данные отправляются');
                 },
-                success : function()
-                {
+                success: function () {
                     $(".phone-form_title").html("Спасибо за обращение, менеджер свяжется  с вами в ближайшее время");
-                    $('#phone-form').hide();
-
-                    $('.goo_conversion').hide().html('<iframe src="/feedback/goo_conversion?goo"></iframe>');
-                    if(yaID) {  yaID.reachGoal('goal_zapros_0');yaID.reachGoal('goal_zapros_1'); yaID.reachGoal('goal_allgoal_0');  }
-                    ga('send', 'event', 'goal_allgoal_', 'goal_allgoal');
-                    ga('send', 'event', 'goal_zapros_1'+'_', 'goal_zapros_1');
-                    m2p();
+                    $(form).hide();
+                    $('.phone-form_text ').hide();
+                    //код в этом блоке выполняется при успешной отправке сообщения
                 },
-
                 error : function () {
                     $(".phone-form_title").html("Ошибка отправки");
                 }
-
             });
         }
         else {
             $('.phone-form_inp').css({'border': '2px solid #ab1717'});
         }
-
     });
-
-
 });
 
-//$('input[name=phone]').mask("+7 (999) 999-99-99");
+// =====================Рассчет за 30 минут
+$(function(){
 
-    $('#form-30min__link').click( function(event){  //открытие модального окна
+    //открытие модального окна
+    $('#form-30min__link').click( function(event){
         var where = $(this).attr('data-where');
         event.preventDefault();
         $('#form-30min_overlay').fadeIn(400,
@@ -127,19 +120,53 @@ $(function() {
                 $('#form-30min').find('input[name=where]').val(where);
             });
     });
+    /* Зaкрытие мoдaльнoгo oкнa */
+    $('#form-30min_close, #form-30min_overlay').click( function(){
+        $('.form-30min_wrap')
+            .animate({opacity: 0, top: '45%'}, 200,
+                function(){
+                    $(this).css('display', 'none');
+                    $('#form-30min_overlay').fadeOut(400);
+                }
+            );
+    });
+    //устанавливаем событие отправки для формы
+    $(".form-30min_btn").click(function(e) {
+        e.preventDefault();
+        var form = $(this).parent('form');
+        var ph = $('.form-30min_inp-phone');
+        var ph_ = ph.val();
 
+        if (ph_!='') {
+            $('.form-30min_inp-phone').css({'border': '1px solid #c9c9c9'});
 
-
-/* Зaкрытие мoдaльнoгo oкнa */
-$('#form-30min_close, #form-30min_overlay').click( function(){
-    $('.form-30min_wrap')
-        .animate({opacity: 0, top: '45%'}, 200,
-            function(){
-                $(this).css('display', 'none');
-                $('#form-30min_overlay').fadeOut(400);
-            }
-        );
+            var form_data = form.serialize();
+            $.ajax({
+                type: "POST",
+                url: "http://maot.ru/podmsk/tilda/les_landing.php",
+                data: form_data,
+                beforeSend: function () {
+                    $(".form-30min_btn").attr('disabled', true);
+                    $(":submit", this).attr("disabled", "disabled");
+                    $(".form-30min_title").html('Данные отправляются');
+                },
+                success: function () {
+                    $(".form-30min_title").html("Спасибо за обращение, менеджер свяжется  с вами в ближайшее время");
+                    $('.form-30min_title').css({'background': '#4282b9'});
+                    $(form).hide();
+                },
+                error : function () {
+                    $(".form-30min_title").html("Ошибка отправки");
+                }
+            });
+        }
+        else {
+            $('.form-30min_inp-phone').css({'border': '2px solid #ab1717'});
+        }
+    });
 });
+// =====================Рассчет за 30 минут - END
+
 
 $('.popup-container_img').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
@@ -163,4 +190,15 @@ $(window).resize(function(){
     }
 });
 
+jQuery(function(){
+    if ($(window).width() <= 1139) {
+        $('.nav').singlePageNav({
+            offset: 100
+        });
+    } else {
+        $('.nav').singlePageNav({
+            offset: 80
+        });
+    }
+});
 
